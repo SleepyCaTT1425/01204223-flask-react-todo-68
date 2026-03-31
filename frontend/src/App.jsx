532ead858dd68e92b79c43a4a8b94cf2031922f7
SleepyCaTT1425 <pannawitchakorn14@gmail.com>
2026-03-31 19:14:18 +0700

    todo-deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,42 +7,43 @@ import LoginForm from './LoginForm.jsx';
 import PrivateRoute from './PrivateRoute.jsx';
 
 function App() {
-  const TODOLIST_API_URL = 'http://localhost:5000/api/todos/';
-  const TODOLIST_LOGIN_URL = 'http://localhost:5000/api/login/';
+    //   const TODOLIST_API_URL = 'http://localhost:5000/api/todos/';
+    //   const TODOLIST_LOGIN_URL = 'http://localhost:5000/api/login/';
+    const TODOLIST_API_URL = '/api/todos/';
+    const TODOLIST_LOGIN_URL = '/api/login/';
 
-  return (
-    // <TodoList apiUrl={TODOLIST_API_URL}/>
-    <AuthProvider>
-        <BrowserRouter>
-            <Routes>
-                <Route 
-                path="/" 
-                element={
-                    <PrivateRoute>
-                        <TodoList apiUrl={TODOLIST_API_URL}/>
-                    </PrivateRoute>
-                } 
-                />
-                <Route 
-                path="/about" 
-                element={
-                    <>
-                    <h1>About</h1>
-                    <p>This is a simple todo list application built with React and Flask.</p>
-                    <a href="/">Back to Home</a>
-                    </>
-                } 
-                />
-                <Route
-                path="/login"
-                element={
-                    <LoginForm loginUrl={TODOLIST_LOGIN_URL} />
-                }
-                />
-            </Routes>
-        </BrowserRouter>
-    </AuthProvider>
-  )
+    return (
+        <AuthProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <PrivateRoute>
+                                <TodoList apiUrl={TODOLIST_API_URL} />
+                            </PrivateRoute>
+                        }
+                    />
+                    <Route
+                        path="/about"
+                        element={
+                            <>
+                                <h1>About</h1>
+                                <p>This is a simple todo list application built with React and Flask.</p>
+                                <a href="/">Back to Home</a>
+                            </>
+                        }
+                    />
+                    <Route
+                        path="/login"
+                        element={
+                            <LoginForm loginUrl={TODOLIST_LOGIN_URL} />
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+    )
 }
 
 export default App
